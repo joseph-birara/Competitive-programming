8d20473059b4eeb42099825759f3bf393976d13c
joseph-birara <93038960+joseph-birara@users.noreply.github.com>
2022-07-19 01:15:08 +0300

    Time: 54 ms (32.86%), Space: 13.7 MB (97.77%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,8 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        import math
+        return math.pow(x,n)
+         
+                       
+                       
+        
