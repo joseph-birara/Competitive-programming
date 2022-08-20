4db3d27b5674afefc40fdf80cdcc4a85b1eaf3a4
joseph-birara <josephbirara92@gmail.com>
2022-08-20 11:33:21 +0300

    contest solutions added

---
+++ codeforce/kingEScap.py
@@ -0,0 +1,43 @@
+n = int(input())
+a ,b = map(int,input().split())
+c ,d = map(int,input().split())
+e ,f = map(int,input().split())
+
+	
+	
+	
+if(c<a and e<a and d<b and f<b):
+
+    if(e-a!=f-b):
+        print("YES")
+    else:
+        print("NO")
+
+elif(c<a and e<a and d>b and f>b):
+    
+
+    if(e-a!=f-b):
+    
+        print("YES")
+    else:
+        print("NO")
+
+elif(c>a and e>a and d<b and f<b):
+
+    if(e-a!=f-b):
+        print("YES")
+    else:
+        print("NO")
+
+elif(c>a and e>a and d>b and f>b):
+    
+
+    if(e-a!=f-b):
+        
+        print("YES")
+    else:
+        print("NO")
+
+else:
+    
+    print("NO")
