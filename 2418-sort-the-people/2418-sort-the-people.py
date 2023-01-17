79a6517fbaa667d166e0ebf63f360dba2937c86d
joseph <93038960+joseph-birara@users.noreply.github.com>
2023-01-17 19:33:13 +0300

    Time: 1434 ms (5.53%), Space: 14.2 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,26 +1,20 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         
-        d = {}        
+              
          
         length = len(names)
         
-        for index in range (length):
-            d[heights[index]] = names[index]
+        for index in range (1,length):
+            j = index
+            while j >0 and heights[j] >heights[j-1]:
+                heights[j], heights[j-1] = heights[j-1],heights[j]
+                names[j], names[j-1] = names[j-1],names[j]
+                j -=1
+            
         
-        isSwaped = True
+        return names
         
-        for i in range(length-1):
-            for j in range(length-(i+1)):
-                if heights[j] < heights[j+1]:
-                    heights[j], heights[j+1] = heights[j+1],heights[j]
-                    isSwaped = False
-            # if not isSwaped:
-            #     break
-        temp = []
-        for item in heights:
-            temp.append(d[item])
-        return temp
                 
                 
         
