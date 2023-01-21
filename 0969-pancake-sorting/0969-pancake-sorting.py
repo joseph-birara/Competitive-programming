db04ae8e630c86f9d60c03bc6dad59e9bc508cea
joseph <93038960+joseph-birara@users.noreply.github.com>
2023-01-21 11:37:31 +0300

    Time: 50 ms (54.96%), Space: 13.9 MB (27.88%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        result = []
+        def flip(idx):
+            for i in range(0, idx//2+1):
+                tmp = arr[i]
+                arr[i] = arr[idx-i]
+                arr[idx-i] = tmp
+        for i in range(len(arr)-1, 0, -1):
+            for j in range(1, i+1):
+                if arr[j] == i+1:
+                    flip(j)
+                    result.append(j+1)
+                    break
+            flip(i)
+            result.append(i+1)
+        return result
+        
+        
+        
