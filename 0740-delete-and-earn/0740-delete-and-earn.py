af3e4182e22cf5731f33e061197753ee69b53650
joseph Birara <93038960+joseph-birara@users.noreply.github.com>
2023-08-05 23:11:45 +0300

    Time: 73 ms (99.07%), Space: 18.5 MB (62.04%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,12 +5,10 @@ class Solution:
         freq_table = {}
         for num in nums:
             freq_table[num] = freq_table.get(num, 0) + 1
-
-        # Initialize variables for maximum points when including or excluding the current number
+      
         prev_points = 0
         curr_points = 0
-
-        # Iterate through the frequency table and calculate maximum points
+        
         for num in sorted(freq_table):
             if num - 1 not in freq_table:
                 prev_points, curr_points = curr_points, curr_points + num * freq_table[num]
