c9569745c10373e1543ea1b3ce92558dd927837e
joseph Birara <93038960+joseph-birara@users.noreply.github.com>
2025-03-11 09:08:58 +0300

    Time: 363 ms (8.96%), Space: 17.8 MB (67.33%) - LeetHub

---
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        left = right = total = 0       
+        freq = [0] * 3
+        while right < len(s):            
+            freq[ord(s[right]) - ord("a")] += 1            
+            while self.has_all(freq):               
+                total += len(s) - right                
+                freq[ord(s[left]) - ord("a")] -= 1
+                left += 1
+
+            right += 1
+
+        return total
+
+    def has_all(self, freq: list) -> bool:        
+        return all(f > 0 for f in freq)
